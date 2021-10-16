6f95716bf2991894d630b525a2e53bec25f27995
piyush1106 <76770482+piyush1106@users.noreply.github.com>
2021-10-16 12:05:05 +0530

    Update player.js

--- player.js
+++ player.js
@@ -1,28 +1,47 @@
 class Player {
-    constructor(){
-        this.index=null
-        this.distance=0
-        this.name=null
+  constructor(){
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
+    this.rank = null;
+  }
 
-    }
-    getCount(){
-        var playercountref = db.ref("playerCount")
-        playercountref.on("value",(data)=>{
-            playerCount=data.val()
-        })
-    }
-    updateCount(count){
-        db.ref("/").update({playerCount:count})
-    }
-    update(){
-        var playerindex="players/player"+this.index
-        db.ref(playerindex).set({name:this.name,distance:this.distance})
-    }
-    static getplayerinfo(){
-        var playerinforef=db.ref("players")
-        playerinforef.on("value",(data)=>{
-            allplayers=data.val()
-        })
-    }
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance
+    });
+  }
+
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+  getCarsAtEnd (){
+    var CarsAtEndRef = database.ref('CarsAtEnd');
+    CarsAtEndRef.on("value",(data)=>{
+      this.rank = data.val();
+    })
+  }
+  static updateCarsAtEnd(rank){
+    database.ref('/').update({
+      CarsAtEnd:rank
+    })
+  }
 }
- 
